ca373aab6145c3d6f3921d33588ff8a10c88cd37
Mokhtar Fertit <mokhtarfertit@gmail.com>
2026-07-18 19:43:58 +0100

    update of main .sh for find project directory

--- main.sh
+++ main.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CONFIG_PATH="$BASE_DIR/config/collector.conf"
+HELPER_PATH="$BASE_DIR/utils/helpers.sh"
 
-source "$PROJECT_DIR/utils/helpers.sh"
-
+#Start the script 
+source $HELPER_PATH
 write_report_header "Linux Incident Response Collector"
+
