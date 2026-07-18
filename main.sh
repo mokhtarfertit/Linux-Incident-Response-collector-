f81d7aa955941507e1b12781dee3c4dead780e23
Mokhtar Fertit <mokhtarfertit@gmail.com>
2026-07-18 19:25:41 +0100

    update of main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
-#!/bin/bash 
+#!/usr/bin/env bash
 
-source "$LIRC/utils/helpers.sh"
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "$PROJECT_DIR/utils/helpers.sh"
 
 write_report_header "Linux Incident Response Collector"
