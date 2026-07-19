850e426392795c5f78131dd4f39e56e6ed052cb0
Mokhtar Fertit <mokhtarfertit@gmail.com>
2026-07-19 18:27:23 +0100

    i think this is last version of update check required

--- main.sh
+++ main.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CONFIG_PATH="$BASE_DIR/config/collector.conf"
+CONFIG_PATH="$BASE_DIR/config/config.collector.conf"
 HELPER_PATH="$BASE_DIR/utils/helpers.sh"
 
+#check requret of project
+if [[ ! -f "$CONFIG_PATH" || ! -f "$HELPER_PATH" ]]; then
+	echo "A required file is missing"
+	exit 1
+fi
+
 #Start the script 
-source $HELPER_PATH
+source "$HELPER_PATH"
 write_report_header "Linux Incident Response Collector"
 
