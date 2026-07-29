a99c2c7aa36a9ab95814d9970cf1a5e317d8ff6e
Mokhtar Fertit <mokhtarfertit@gmail.com>
2026-07-29 12:43:08 +0100

    in part of modules we implement logged user

--- modules/user.sh
+++ modules/user.sh
@@ -1,1 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+collect_current_logged() {
+
+	printf '=%.0s' {1..70}
+	echo 
+	echo "Collect currently loged "
+	printf '=%.0s' {1..70}
+	echo 
+	
+	echo "** current loger is :" 
+	who 
+	echo "** all loged users:"
+	last
+
+}
