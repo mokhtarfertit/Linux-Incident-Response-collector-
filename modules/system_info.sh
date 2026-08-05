77e9ec754e8deedd038c6f00bf20c978edd2b783
Mokhtar Fertit <mokhtarfertit@gmail.com>
2026-08-05 14:06:11 +0100

    we update modules user, processes, ..

--- modules/system_info.sh
+++ modules/system_info.sh
@@ -7,7 +7,70 @@ collect_system_info() {
 	printf '=%.0s' {1..70}
 	echo 
 
-	hostnamectl
+	echo 
+	echo "--- HOSTNAME ---"
+	if ! hostname; then 
+		echo "Error: failed to collect hostname" >&2
+		return 1
+	fi
+
+	echo 
+	echo "--- KERNEL INFORMATION ---"
+	if ! uname -a; then 
+		echo "Error: failed to collect kernel information" >&2
+		return 1
+	fi
+
+	echo 
+	echo "--- OPERATING SYSTEM ---"
+	if [[ -r /etc/os-release ]]; then
+		cat /etc/os-release
+	else
+		echo "/etc/os-release is not available"
+	fi
+
+	echo 
+	echo "-- CURRENT UTC TIME ---"
+	date -u "+%Y-%m-%d %H:%M:%SZ"
+
+	echo 
+	echo "--- SYSTEM UPTIME ---"
+	uptime 
+
+	echo 
+	echo "--- HOSTNAEMCTL INFORMATION ---"
+	if command_exists hostnamectl; then
+		if ! hostnamectl;then
+			echo "hostname exists but could not collect information"
+		fi
+	else
+		echo "hostnamectil is not available"
+	fi
+
+	echo "--- CPU INFORMATION ---"
+	if command_exists lscpu; then
+		lscpu
+	else
+		echo "lscpu is not availalble"
+	fi
+
+	echo 
+	echo "--- MEMORY INFORAMTION ---"
+	if command_exists free; then
+		free -h
+	else
+		echo "free is not available"
+	fi
+
+	echo 
+	echo "--- BLOCK DEVICES ---"
+	if command-exists lsblk; then
+		lsblk -f
+	else
+		echo "lsblk is not available"
+	fi
+
+	return 0
 
 
 }
--- modules/user.sh
+++ modules/user.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 collect_current_logged() {
+	local max_records="${MAX_LOGIN_RECORDS:-50}"
 
 	printf '=%.0s' {1..70}
 	echo 
@@ -8,9 +9,40 @@ collect_current_logged() {
 	printf '=%.0s' {1..70}
 	echo 
 	
-	echo "** current loger is :" 
-	who 
-	echo "** all loged users:"
-	last
+	if ! [[ "$max_records" =~ ^[0-9]+$ ]] ||
+		(( 10#max_records < 1))
+	then
+		echo "Error: MAZ_LOGIN_RECORDS must be a positive number" >&2
+		return 1
+	fi
 
+	max_records=$((10#$max_records))
+
+	echo 
+	echo "--- CURRENTLY LOGGED-IN USERS ---"
+
+	if command_exists who; then 
+		if ! who -a; then
+			echo "Erro: failed to collect current sessions" >&2
+			return 1
+		fi
+	else
+		echo "Error: who command is not available" >&2
+		return 1
+	fi
+
+	echo "--- RECENT LOGIN RECORDS ---"
+	echo "Maximum records: $max_records"
+
+	if command_exists last; then
+		if ! last -n "$max_records"; then
+			echo "Error: failed to collect login history" >&2
+			return 1
+		fi 
+	else
+		echo "Error: last command is not available" >&2
+		return 1
+	fi 
+
+	return 0
 }
